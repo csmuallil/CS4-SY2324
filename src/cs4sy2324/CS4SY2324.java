6c5dd6474b47104b09f1964d57e75c01b3f82ea5
csmuallil <112594397+csmuallil@users.noreply.github.com>
2023-09-05 14:29:21 +0800

    Update CS4SY2324.java

--- src/cs4sy2324/CS4SY2324.java
+++ src/cs4sy2324/CS4SY2324.java
@@ -14,7 +14,34 @@ public class CS4SY2324 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String anime1 = "Sword Art Online";
+        double rating1 = 4.0;
+        int noOfSeasons1 = 4;
+        
+        String anime2 = "Attack On Titan";
+        double rating2 = 4.3;
+        int noOfSeasons2 = 4;
+        
+        String anime3 = "Boku No Hero Academia";
+        double rating3 = 4.1;
+        int noOfSeasons3 = 5;
+        
+        System.out.println("Anime name: " + anime1);
+        System.out.println("Rating: " + rating1);
+        System.out.println("Number of Seasons: " + noOfSeasons1);
+        
+        System.out.println("");
+        
+        System.out.println("Anime name: " + anime2);
+        System.out.println("Rating: " + rating2);
+        System.out.println("Number of Seasons: " + noOfSeasons2);
+        
+        System.out.println("");
+        
+        System.out.println("Anime name: " + anime3);
+        System.out.println("Rating: " + rating3);
+        System.out.println("Number of Seasons: " + noOfSeasons3);
     }
     
 }
